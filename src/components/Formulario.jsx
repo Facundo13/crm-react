7651b36ecea2d26735d18131fceeff58785b8bf5
FacundoEstrada <facundoestrada1015@gmail.com>
2022-03-22 15:25:39 -0300

    fin proyecto

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -27,16 +27,32 @@ const Formulario = ({ cliente, cargando }) => {
 
     const handleSubmit = async (valores) => {
         try {
-            const url = 'http://localhost:4000/clientes'
+            let respuesta;
+            if (cliente.id){
+                // Editando un registro
+                const url = `http://localhost:4000/clientes/${cliente.id}`
 
-            const respuesta = await fetch(url, {
-                method: 'POST',
-                body: JSON.stringify(valores),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-            const resultado = await respuesta.json();
+                respuesta = await fetch(url, {
+                    method: 'PUT',
+                    body: JSON.stringify(valores),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+            } else{
+                //Nuevo registro
+                const url = 'http://localhost:4000/clientes'
+
+                respuesta = await fetch(url, {
+                    method: 'POST',
+                    body: JSON.stringify(valores),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+            }
+
+            await respuesta.json();
             navigate('/clientes')
         } catch (error) {
             console.log(error)
